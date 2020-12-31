94d02b386872fb65e30f61b5396fa0e44737fb09
Christian Henry <chenry5724@gmail.com>
2020-12-31 12:19:33 -0500

    deploy fix socket config

--- s2meclient/src/Atoms.js
+++ s2meclient/src/Atoms.js
@@ -3,7 +3,7 @@ import {atom} from 'jotai'
 
 // export 
 
-export const endpoint=atom(process.env.Port || 'http://127.0.0.1:3001')
+// export const endpoint=atom(process.env.Port || 'http://127.0.0.1:3001')
 export const loggedIn =atom(false)
 export const user =atom('')
 export const messages=([{msg:'',username:'',time:''}])
